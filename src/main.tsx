2ba7f58601ef62e63f1042cc78e9756260ec929c
yassin <yassin.lahhit@outlook.com>
2026-03-03 09:29:42 +0100

    speedInsight 3

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,7 @@
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
+import { SpeedInsights } from "@vercel/speed-insights/react"
+
 
 createRoot(document.getElementById("root")!).render(<App />);
